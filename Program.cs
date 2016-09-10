571e3cecae328798c522fb530a1118c8339aebfc
Иван Кудрявцев <kivinew@hotmail.com>
2016-09-10 23:31:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*Создание сервера*/
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpProtocol
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // создаём конечную локальную точку подключения
+            var ipHost = Dns.GetHostEntry("localhost");
+            var ipAddress = ipHost.AddressList[0];
+            var ipEndPoint = new IPEndPoint(ipAddress, 9989);
+            // создаём сокет
+            try
+            {
+                var sListener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // связываем его с точкой подключения
+                sListener.Bind(ipEndPoint);
+                // запускаем прослушивание
+                sListener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Start connection");
+                    // ожидаем подключения клиента
+                    var handler = sListener.Accept();
+
+                    var bytes = new byte[1024];
+                    // принимаем от него сообщение
+                    var bytesRec = handler.Receive(bytes);
+                    var data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    Console.WriteLine(data);
+                    const string theReply = "Thanks!";
+                    var msg = Encoding.ASCII.GetBytes(theReply);
+                    // отправляем ответ
+                    handler.Send(msg);
+                    // и завершаем сеанс
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nError: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ tcpClient/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Создание клиента*/
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpClient
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            byte[] bytes = new byte[1024];
+            // конечная точка подключения
+            var ipHost = Dns.Resolve("localhost");
+            var ipAddress = ipHost.AddressList[0];
+            var ipEndPoint = new IPEndPoint(ipAddress, 9989);
+            // создаём сокет
+            var sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // устанавливаем соединение
+                sender.Connect(ipEndPoint);
+                // отправляем сообщение
+                const string msg = "Hello server!";
+                var bytesSend = sender.Send(Encoding.ASCII.GetBytes(msg));
+                // получаем ответ от сервера
+                var bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Server:" + Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                // завершаем работу
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: "+ex/*.Message*/);
+            }
+        }
+    }
+}
